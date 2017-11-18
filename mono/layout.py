e487ae7e31456e2d6f8be7e8923d3c9f8e559b8b
Hamza Haiken <hamza.haiken@ordina.nl>
2017-11-18 09:49:55 +0100

    Start book object and viewer

--- mono/layout.py
+++ mono/layout.py
@@ -1,53 +1,62 @@
-from .util.lang import auto_properties
+from .util.lang import auto_properties, auto_repr
 
 
+@auto_repr
 class TableSettings(object):
     @auto_properties
     def __init__(self,
-        table_of_contents=False,
-        list_of_figures=False,
-        list_of_tables=False,
-        list_of_code_listings=False,
+        table_of_contents=None,
+        list_of_figures=None,
+        list_of_tables=None,
+        list_of_code_listings=None,
         chapter_numbering=True,
         fill_character=".",
         fill_spacing=1
     ): pass
 
 
+@auto_repr
 class BookSettings(object):
     @auto_properties
     def __init__(self,
         tables=None,
+        size=(80, 120),
         chapter_numbering=True,
         code_listings_line_numbering=True,
         code_listings_syntax_highlighting=True,
     ): pass
 
 
+@auto_repr
 class NumberMappers(object):
     default = lambda n: str(n)
-    roman = raise NotImplementedError()
-    roman_lower = raise NotImplementedError()
-    alphabetical = raise NotImplementedError()
+    @property
+    def roman(self): raise NotImplementedError()
+    @property
+    def roman_lower(self): raise NotImplementedError()
+    @property
+    def alphabetical(self): raise NotImplementedError()
 
 
+@auto_repr
 class HeaderFooter(object):
     # TODO: In two-sided mode, flip sides
     @auto_properties
     def __init__(self,
         left_cell_writer=None,
         middle_cell_writer=None,
-        right_cell_writer=None
+        right_cell_writer=None,
+        separator_line=False
     ): pass
 
     @staticmethod
-    def with_page_number(self,
-                        left_cell_writer=None,
-                        middle_cell_writer=None,
-                        right_cell_writer=None,
-                        page_number_mapper=NumberMappers.default,
-                        center_page_number=False):
-
+    def with_page_number(
+        left_cell_writer=None,
+        middle_cell_writer=None,
+        right_cell_writer=None,
+        page_number_mapper=NumberMappers.default,
+        center_page_number=False
+    ):
         page_number_writer = lambda context: page_number_mapper(context.page_number)
 
         if center_page_number:
@@ -56,14 +65,28 @@ class HeaderFooter(object):
             return HeaderFooter(left_cell_writer, center_page_number, page_number_writer)
 
 
+@auto_repr
 class PageSettings(object):
     @auto_properties
     def __init__(self,
-        size=(80, 120),
-        marigns=(5, 2, 5, 2),
+        margins=(5, 2, 5, 2),
         header=None, footer=None,
         two_sided=True,
         page_number=True,
         page_number_mapper=NumberMappers.default,
         center_page_number=False
-    ): pass
+    ):
+        if page_number:
+            if footer:
+                self.footer = HeaderFooter.with_page_number(
+                    left_cell_writer=footer.left_cell_writer,
+                    middle_cell_writer=footer.middle_cell_writer,
+                    right_cell_writer=footer.right_cell_writer,
+                    page_number_mapper=page_number_mapper,
+                    center_page_number=center_page_number
+                )
+            else:
+                self.footer = HeaderFooter.with_page_number(
+                    page_number_mapper=page_number_mapper,
+                    center_page_number=center_page_number
+                )
--- mono/util/lang.py
+++ mono/util/lang.py
@@ -7,8 +7,49 @@ def auto_properties(initializer):
 
    @wraps(initializer)
    def wrapper(self, *args, **kwargs):
-       for name, arg in zip(names[1:], args) + kwargs.items():
+       for name, arg in list(zip(names[1:], args)) + list(kwargs.items()):
            setattr(self, name, arg)
-       initializer(self, *args, **kargs)
+       initializer(self, *args, **kwargs)
 
    return wrapper
+
+
+def auto_repr(Class):
+    class Representable(Class):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def __repr__(self):
+            return repr(vars(self))
+
+    return Representable
+
+
+def indent_json_like(string, indent=2):
+    level = 0
+    result = ""
+    in_quotes = False
+    in_parens = False
+    for char in string:
+        result += char
+        if char == "'":
+            in_quotes = not in_quotes
+        elif char == "(":
+            in_parens = True
+        elif char == ")":
+            in_parens = False
+
+        if char == "{" or char == "[":
+            level += 1
+            result += "\n"
+            result += " " * (indent * level)
+        elif char == "}" or char == "]":
+            level -= 1
+            result = result[:-1]
+            result += "\n"
+            result += " " * (indent * level)
+            result += char
+        elif char == "," and not in_quotes and not in_parens:
+            result += "\n"
+            result += " " * (indent * level - 1)
+    return result
