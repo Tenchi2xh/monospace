97537ad590b94a47ff97920c699c43b4989b9f89
Hamza Haiken <hamza.haiken@ordina.nl>
2017-12-20 20:33:41 +0100

    Add Link type and warning when missing format

--- mono/formatting/terminal.py
+++ mono/formatting/terminal.py
@@ -6,7 +6,12 @@ def convert(tag):
         code = "1m" if tag.start else "22m"
     elif tag.type == "Emphasis":
         code = "3m" if tag.start else "23m"
+    elif tag.type == "Link":
+        code = "4m" if tag.start else "24m"
     elif tag.type == "InlineCode":
         code = "7m" if tag.start else "27m"
+    else:
+        print("MISSING: " + tag.type)
+        return ""
 
     return csi + code
