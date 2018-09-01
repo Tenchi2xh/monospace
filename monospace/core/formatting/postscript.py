be3dd917d15b7ffae257d0cecaaefafc1552a9bf
Hamza Haiken <tenchi@team2xh.net>
2018-09-01 16:40:26 +0200

    Implement basic styles in postscript formatter

--- monospace/core/formatting/postscript.py
+++ monospace/core/formatting/postscript.py
@@ -1,11 +1,20 @@
 import pkg_resources
 from jinja2 import Template
-from typing import List, Union
+from typing import List, Union, Set
 from .formatter import Formatter, FormatTag
 from ..domain import Settings
 
 raw_template = pkg_resources.resource_string(__name__, "template.ps")
 prolog_template = Template(raw_template.decode("UTF-8"))
+
+font_styles = {
+    (): "fR",
+    ("Bold",): "fB",
+    ("Italic",): "fI",
+    ("Bold", "Italic"): "fO"
+}
+
+DARK_MODE = True
 
 
 class PostScriptFormatter(Formatter):
@@ -13,11 +22,34 @@ class PostScriptFormatter(Formatter):
 
     @staticmethod
     def format_tags(line: List[Union[FormatTag, str]]) -> str:
-        result = "("
+        # Reset color, regular font, open group
+        result = "%d fg fR (" % (15 if DARK_MODE else 0)
+
+        # For color styles, closing the current group and invoking the next
+        # color is enough.
+        # For bold / italic / bold-italic, they cannot be combined. Instead,
+        # they are separate fonts that have to be selected.
+
+        # TODO: Works for now, but closing two tags in a row will make
+        # an empty group: ') u fO (...) u fB () u fR ('
+
+        current_font_styles: Set[str] = set([])
+
         for elem in line:
             if isinstance(elem, str):
                 result += sanitize(elem)
+            else:
+                tag = elem
+                if tag.kind in ("Bold", "Italic"):
+                    if tag.open:
+                        current_font_styles.add(tag.kind)
+                    else:
+                        current_font_styles.remove(tag.kind)
+                    # Close group, render, switch to correct font, open next
+                    key = tuple(sorted(current_font_styles))
+                    result += ") u %s (" % font_styles[key]
 
+        # function u renders each character as unicode, in a regular grid
         return result + ") u "
 
     @staticmethod
@@ -26,7 +58,10 @@ class PostScriptFormatter(Formatter):
 
     @staticmethod
     def begin_page(settings: Settings) -> str:
-        return "tr"
+        result = ""
+        if DARK_MODE:
+            result += "bk "
+        return result + "tr"
 
     @staticmethod
     def format_line(line: str) -> str:
