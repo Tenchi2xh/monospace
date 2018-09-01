d46fe19c7654c83897e746e3a66414943cb28455
Hamza Haiken <tenchi@team2xh.net>
2018-09-01 20:52:37 +0200

    Remove poc

--- poc/mono/context.py
+++
@@ -1,8 +0,0 @@
-from .util.lang import auto_repr
-
-@auto_repr
-class Context(object):
-    def __init__(self, book):
-        self.book = book
-        self.stack = []
-        self.chapter = [0 for _ in range(10)]
--- poc/mono/viewer.py
+++
@@ -1,8 +0,0 @@
-from .cursebox import *
-
-
-def view(book):
-    with Cursebox() as cb:
-        width, height = cb.width, cb.height
-
-        cb.poll_event()
