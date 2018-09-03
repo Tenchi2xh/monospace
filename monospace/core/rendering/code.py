e5f76eebc2ba4d96301b2bf62517ab77c9034990
Hamza Haiken <tenchi@team2xh.net>
2018-09-03 18:22:33 +0200

    Implement syntax highlighting

--- monospace/core/rendering/code.py
+++ monospace/core/rendering/code.py
@@ -1,9 +1,12 @@
-# from pygments.lexers import get_lexer_by_name  # type: ignore
 import re
 from typing import List, Union
+from pygments.lexers import get_lexer_by_name  # type: ignore
+from pygments.styles import get_style_by_name  # type: ignore
+from pygments.token import Token  # type: ignore
+from pygments.util import ClassNotFound  # type: ignore
 
 from ..domain import document as d
-from ..formatting import Formatter, FormatTag
+from ..formatting import Formatter, FormatTag, Format as F
 
 
 def highlight_code_block(
@@ -11,9 +14,53 @@ def highlight_code_block(
     formatter: Formatter,
     width: int
 ) -> List[str]:
-    lines = code_block.code.splitlines()
-    # Keep the whitespace, this is source code!
-    words = [re.split(r"(\s+)", line) for line in lines]
+    try:
+        lexer = get_lexer_by_name(code_block.language)
+    except ClassNotFound:
+        lexer = None
+
+    # Pygments styles are in this format:
+    # ['fg hex', bold, nobold, italic, noitalic, ul, noul, 'bg hex',
+    #  border, roman, sans, mono]
+    # (https://github.com/nex3/pygments/blob/master/pygments/style.py#L49)
+    # hex values have no leading '#'
+    style = get_style_by_name("monokai")  # TODO: Make this a setting + meta
+
+    words: List[List[Union[FormatTag, str]]] = [[]]
+
+    if lexer:
+        for e in list(lexer.get_tokens(code_block.code)):
+            # Map pygments styles for each token to a list of FormatTag
+            style_map = {}
+            for token_type, values in style._styles.items():
+                tags = []
+                if values[0]:
+                    tags.append(FormatTag(
+                        kind=F.ForegroundColor,
+                        data={"color": "#" + values[0]}
+                    ))
+                style_map[token_type] = tags
+
+        tokens = lexer.get_tokens(code_block.code)
+        last_line = words[-1]
+        for token, word in tokens:
+            if (token, word) == (Token.Text, "\n"):
+                words.append([])
+                last_line = words[-1]
+                continue
+
+            tags = style_map[token]
+            last_line.extend(tags)
+            last_line.append(word)
+            last_line.extend([tag.close_tag for tag in tags])
+
+        # Remove trailing empty line
+        if not last_line:
+            words.pop()
+    else:
+        lines = code_block.code.splitlines()
+        # Keep the whitespace, this is source code!
+        words = [re.split(r"(\s+)", line) for line in lines]  # type: ignore
 
     wrapped: List[List[Union[FormatTag, str]]] = []
     current_length = 0
@@ -33,6 +80,9 @@ def highlight_code_block(
         last_line = wrapped[-1]
 
         for word in line:
+            if isinstance(word, FormatTag):
+                last_line.append(word)
+                continue
             if current_length + len(word) > width:
                 new_line()
                 last_line = wrapped[-1]
