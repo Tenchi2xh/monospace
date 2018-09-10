f3a1913ddb33886194174ba61388db34b117e277
Hamza Haiken <tenchi@team2xh.net>
2018-09-10 12:48:24 +0200

    Second pass of refactoring

--- monospace/core/rendering/images.py
+++ monospace/core/rendering/images.py
@@ -41,52 +41,17 @@ def ansify(
         image = original
 
     pixels = image.convert("RGBA").load()
-    lines: List[str] = []
 
     if mode == Mode.Super:
-        return superify(image, pixels, format_func)
+        render = superify
+    elif mode == Mode.Pixels:
+        render = pixelify
+    elif mode == Mode.Dithered and palette != Palette.RGB:
+        render = ditherify
+    else:
+        render = blockify
 
-    # If height is odd, forget about the last line
-    for y in range(0, image.height - (image.height % 2), 2):
-        line = []
-        for x in range(image.width):
-            if mode == Mode.Pixels:
-                t1 = color_tag(pixels, x, y, palette, background=True)
-                t2 = color_tag(pixels, x, y + 1, palette)
-                line.extend([t1, t2, "▄", t2.close_tag, t1.close_tag])
-
-            elif mode == Mode.Dithered and palette != Palette.RGB:
-                c0 = average_color(pixels, x, y)
-                c1, c2 = n_closest_color(2, c0, palettes[palette])
-
-                d01 = sqrt(distance(c0, c1))
-                d12 = sqrt(distance(c1, c2))
-                d02 = sqrt(distance(c0, c2))
-
-                if d02 > d01 + d12 or d12 == 0:
-                    c2 = c1
-                    block = "█"
-                else:
-                    factor = d01 / d12
-                    if factor >= 1:
-                        factor = 0.99
-                    block = " ░▒▓▓"[int(factor * 5)]
-
-                hex1, hex2 = rgb_to_hex(*c1), rgb_to_hex(*c2)
-                t1 = FormatTag(kind=F.ForegroundColor, data={"color": hex2})
-                t2 = FormatTag(kind=F.BackgroundColor, data={"color": hex1})
-                line.extend([t1, t2, block, t2.close_tag, t1.close_tag])
-
-            else:
-                c = average_color(pixels, x, y)
-                if palette != Palette.RGB:
-                    c = n_closest_color(1, c, palettes[palette])[0]
-                h = rgb_to_hex(*c)
-                t = FormatTag(kind=F.ForegroundColor, data={"color": h})
-                line.extend([t, "█", t.close_tag])
-        lines.append(format_func(line))
-
-    return lines
+    return render(image, pixels, palette, format_func)
 
 
 def n_closest_color(n, color, palette):
@@ -131,7 +96,8 @@ def brightness(color):
     return sum(color[:3]) / 3
 
 
-def superify(image, pixels, format_func):
+def superify(image, pixels, palette, format_func):
+    # FIXME: Respect palette
     lines = []
     for y in range(0, image.height - (image.height % 4), 4):
         line = []
@@ -214,4 +180,60 @@ def superify(image, pixels, format_func):
 
         lines.append(format_func(line))
 
+    return lines
+
+
+def pixelify(image, pixels, palette, format_func):
+    lines: List[str] = []
+    for y in range(0, image.height - (image.height % 2), 2):
+        line = []
+        for x in range(image.width):
+            t1 = color_tag(pixels, x, y, palette, background=True)
+            t2 = color_tag(pixels, x, y + 1, palette)
+            line.extend([t1, t2, "▄", t2.close_tag, t1.close_tag])
+        lines.append(format_func(line))
+    return lines
+
+
+def ditherify(image, pixels, palette, format_func):
+    lines: List[str] = []
+    for y in range(0, image.height - (image.height % 2), 2):
+        line = []
+        for x in range(image.width):
+            c0 = average_color(pixels, x, y)
+            c1, c2 = n_closest_color(2, c0, palettes[palette])
+
+            d01 = sqrt(distance(c0, c1))
+            d12 = sqrt(distance(c1, c2))
+            d02 = sqrt(distance(c0, c2))
+
+            if d02 > d01 + d12 or d12 == 0:
+                c2 = c1
+                block = "█"
+            else:
+                factor = d01 / d12
+                if factor >= 1:
+                    factor = 0.99
+                block = " ░▒▓▓"[int(factor * 5)]
+
+            hex1, hex2 = rgb_to_hex(*c1), rgb_to_hex(*c2)
+            t1 = FormatTag(kind=F.ForegroundColor, data={"color": hex2})
+            t2 = FormatTag(kind=F.BackgroundColor, data={"color": hex1})
+            line.extend([t1, t2, block, t2.close_tag, t1.close_tag])
+        lines.append(format_func(line))
+    return lines
+
+
+def blockify(image, pixels, palette, format_func):
+    lines: List[str] = []
+    for y in range(0, image.height - (image.height % 2), 2):
+        line = []
+        for x in range(image.width):
+            c = average_color(pixels, x, y)
+            if palette != Palette.RGB:
+                c = n_closest_color(1, c, palettes[palette])[0]
+            h = rgb_to_hex(*c)
+            t = FormatTag(kind=F.ForegroundColor, data={"color": h})
+            line.extend([t, "█", t.close_tag])
+        lines.append(format_func(line))
     return lines
