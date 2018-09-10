5df1fefba31595fb428326edcbc0cc66b7433a0e
Hamza Haiken <tenchi@team2xh.net>
2018-09-10 12:13:14 +0200

    First pass of refactoring images.py

--- monospace/core/rendering/images.py
+++ monospace/core/rendering/images.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from math import sqrt
-from typing import List
+from typing import List, Sequence
 from heapq import nsmallest
 from PIL import Image  # type: ignore
 from typing import Callable, Optional
@@ -43,112 +43,8 @@ def ansify(
     pixels = image.convert("RGBA").load()
     lines: List[str] = []
 
-    def n_closest_color(n, color, palette):
-        n_closest = nsmallest(
-            n, palette,
-            key=lambda k: distance(color, palette[k])
-        )
-        return [palette[closest] for closest in n_closest]
-
-    def color_tag(pixels, x, y, palette, background=False):
-        r, g, b, _ = pixels[x, y]
-        if palette != Palette.RGB:
-            r, g, b = n_closest_color(1, (r, g, b), palettes[palette])[0]
-        hex_color = rgb_to_hex(r, g, b)
-        return FormatTag(
-            kind=F.BackgroundColor if background else F.ForegroundColor,
-            data={"color": hex_color}
-        )
-
-    def average_color(pixels, x, y):
-        c1 = pixels[x, y]
-        c2 = pixels[x, y + 1]
-        return [int(0.5 * (c1[i] + c2[i])) for i in range(3)]
-
-    def average_color2(*colors):
-        rgbs = zip(*colors)
-        return tuple([int(sum(comp) / len(comp)) for comp in rgbs])
-
-    def brightness(color):
-        return sum(color[:3]) / 3
-
     if mode == Mode.Super:
-        for y in range(0, image.height - (image.height % 4), 4):
-            line = []
-            for x in range(0, image.width - (image.width % 2), 2):
-                c_y0x0 = pixels[x, y]
-                c_y0x1 = pixels[x, y + 1]
-                c_y1x0 = pixels[x + 1, y]
-                c_y1x1 = pixels[x + 1, y + 1]
-
-                c_y0x2 = pixels[x, y + 2]
-                c_y0x3 = pixels[x, y + 3]
-                c_y1x2 = pixels[x + 1, y + 2]
-                c_y1x3 = pixels[x + 1, y + 3]
-
-                tl = average_color2(c_y0x0, c_y0x1)
-                tr = average_color2(c_y1x0, c_y1x1)
-                bl = average_color2(c_y0x2, c_y0x3)
-                br = average_color2(c_y1x2, c_y1x3)
-
-                cats = {}
-
-                bri_tl = brightness(tl)
-                bri_tr = brightness(tr)
-                bri_bl = brightness(bl)
-                bri_br = brightness(br)
-                avg_bri = (bri_tl + bri_tr + bri_bl + bri_br) / 4
-
-                cats[tl] = "a" if bri_tl < avg_bri else "b"
-                cats[tr] = "a" if bri_tr < avg_bri else "b"
-                cats[bl] = "a" if bri_bl < avg_bri else "b"
-                cats[br] = "a" if bri_br < avg_bri else "b"
-
-                c_a = average_color2(*[k for k, v in cats.items() if v == "a"])
-                c_b = average_color2(*[k for k, v in cats.items() if v == "b"])
-
-                if not c_a:
-                    block = " "
-                    c_a = (0, 0, 0)
-                elif not c_b:
-                    block = "█"
-                    c_b = (0, 0, 0)
-                else:
-                    pattern = cats[tl] + cats[tr] + cats[bl] + cats[br]
-                    # We have two colors: there's at least one on each
-                    # side of the averge distance, by nature of an average
-                    mapping = {
-                        "aaab": "▛",
-                        "aaba": "▜",
-                        "aabb": "▀",
-                        "abaa": "▙",
-                        "abab": "▌",
-                        "abba": "▚",
-                        "abbb": "▘",
-                        "baaa": "▟",
-                        "baab": "▞",
-                        "baba": "▐",
-                        "babb": "▝",
-                        "bbaa": "▄",
-                        "bbab": "▖",
-                        "bbba": "▗",
-                    }
-                    block = mapping[pattern]
-
-                t_a = FormatTag(
-                    kind=F.ForegroundColor,
-                    data={"color": rgb_to_hex(*c_a[:3])}
-                )
-                t_b = FormatTag(
-                    kind=F.BackgroundColor,
-                    data={"color": rgb_to_hex(*c_b[:3])}
-                )
-
-                line.extend([t_a, t_b, block, t_a.close_tag, t_b.close_tag])
-
-            lines.append(format_func(line))
-
-        return lines
+        return superify(image, pixels, format_func)
 
     # If height is odd, forget about the last line
     for y in range(0, image.height - (image.height % 2), 2):
@@ -193,5 +89,129 @@ def ansify(
     return lines
 
 
+def n_closest_color(n, color, palette):
+    n_closest = nsmallest(
+        n, palette,
+        key=lambda k: distance(color, palette[k])
+    )
+    return [palette[closest] for closest in n_closest]
+
+
+def color_tag(pixels, x, y, palette, background=False):
+    r, g, b, _ = pixels[x, y]
+    if palette != Palette.RGB:
+        r, g, b = n_closest_color(1, (r, g, b), palettes[palette])[0]
+    hex_color = rgb_to_hex(r, g, b)
+    return FormatTag(
+        kind=F.BackgroundColor if background else F.ForegroundColor,
+        data={"color": hex_color}
+    )
+
+
+def average_color(pixels, x, y):
+    c1 = pixels[x, y]
+    c2 = pixels[x, y + 1]
+    return [int(0.5 * (c1[i] + c2[i])) for i in range(3)]
+
+
+def average(values):
+    return sum(values) / len(values)
+
+
 def rgb_to_hex(r, g, b):
     return '#%02x%02x%02x' % (r, g, b)
+
+
+def average_color2(*colors):
+    rgbs = zip(*colors)
+    return tuple([int(average(comp)) for comp in rgbs])
+
+
+def brightness(color):
+    return sum(color[:3]) / 3
+
+
+def superify(image, pixels, format_func):
+    lines = []
+    for y in range(0, image.height - (image.height % 4), 4):
+        line = []
+        for x in range(0, image.width - (image.width % 2), 2):
+            # We take 2x4 pixels ⣿
+            original = [
+                [pixels[x + i, y + j] for i in range(2)]
+                for j in range(4)
+            ]
+            # We average the first and last two rows together
+            # to get 2x2 pixels (four corners)
+            corners = [
+                average_color2(original[i * 2][j], original[i * 2 + 1][j])
+                for i in range(2)
+                for j in range(2)
+            ]
+            levels = [brightness(corner) for corner in corners]
+            average_level = average(levels)
+
+            # Split corners in 2 groups, depending if they are
+            # lighter or darker than the average brightness
+            group_a: List[Sequence[int]] = []
+            group_b: List[Sequence[int]] = []
+            groups = {}
+            for i in range(len(corners)):
+                color = corners[i]
+                if levels[i] < average_level:
+                    group, letter = group_a, "a"
+                else:
+                    group, letter = group_b, "b"
+                group.append(color)
+                groups[color] = letter
+
+            # Calculate the color for each group
+            # (average of all colors in that group)
+            color_a = average_color2(*group_a)
+            color_b = average_color2(*group_b)
+
+            if not color_a:
+                block = " "
+                color_a = (0, 0, 0)
+            elif not color_b:
+                block = "█"
+                color_b = (0, 0, 0)
+            else:
+                pattern = "".join(
+                    groups[corners[i]]
+                    for i in range(len(corners))
+                )
+                # We have two colors: there's at least one on each
+                # side of the averge distance, by nature of an average
+                mapping = {
+                    "aaab": "▛",
+                    "aaba": "▜",
+                    "aabb": "▀",
+                    "abaa": "▙",
+                    "abab": "▌",
+                    "abba": "▚",
+                    "abbb": "▘",
+                    "baaa": "▟",
+                    "baab": "▞",
+                    "baba": "▐",
+                    "babb": "▝",
+                    "bbaa": "▄",
+                    "bbab": "▖",
+                    "bbba": "▗",
+                }
+                block = mapping[pattern]
+
+            t_a = FormatTag(
+                kind=F.ForegroundColor,
+                data={"color": rgb_to_hex(*color_a[:3])}
+            )
+            t_b = FormatTag(
+                kind=F.BackgroundColor,
+                data={"color": rgb_to_hex(*color_b[:3])}
+            )
+
+            line.extend([t_a, t_b, block, t_a.close_tag, t_b.close_tag])
+
+        lines.append(format_func(line))
+
+    return lines
